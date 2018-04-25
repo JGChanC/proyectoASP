ea61d085d6d672bd4da0faccc1407f6a87c9b8d9
Roger127 <roger_cy@hotmail.com>
2018-04-24 23:31:09 -0500

    Cambios a Perfil y se agrego WebForm de ver noticia

--- Proyecto/Perfil.aspx.designer.cs
+++ Proyecto/Perfil.aspx.designer.cs
@@ -94,13 +94,13 @@ namespace Proyecto {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// TextBox1 control.
+        /// txtmensaje control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.TextBox txtmensaje;
         
         /// <summary>
         /// btnEnviar control.
