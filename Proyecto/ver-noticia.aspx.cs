9eaffcfd5cc02671f8f1a9e35e69923abb776f60
Mario <mariobrap@gmail.com>
2018-05-08 20:41:53 -0500

    Mostrar Comentarios

--- Proyecto/ver-noticia.aspx.cs
+++ Proyecto/ver-noticia.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Proyecto
 {
@@ -17,12 +19,56 @@ namespace Proyecto
             {
                 this.Response.Redirect("Login.aspx");
             }
-            if (this.Session["VSTitulo"] != null)
+           /* if (this.Session["VSTitulo"] != null)
             {
                 lblTitulo.Text = (String) this.Session["VSTitulo"];
                 lblTexto.Text = (String)this.Session["VSTexto"];
                 
+            }*/
+            String id = Convert.ToString(Request.QueryString["id"]);
+            String idUsuario = "";
+            SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\Mario\\Documents\\Aplicaciones_NET\\U4\\conn_bd\\BD_proyecto_post.mdf; Integrated Security = True; Connect Timeout = 30");
+            conn.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Noticias WHERE id="+id+";", conn);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+
+                    lblTitulo.Text = reader["Titulo"].ToString();
+                    lblTexto.Text = reader["Cuerpo"].ToString();
+                    idUsuario = reader["id_usuario"].ToString();
+                }
             }
+            conn.Close();
+            conn.Open();
+            SqlCommand command2 = new SqlCommand("SELECT * FROM Usuarios WHERE id=" + idUsuario + ";", conn);
+            using (SqlDataReader reader = command2.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+
+                    lblUsuario.Text = reader["NombreUsuario"].ToString();
+                }
+            }
+            conn.Close();
+
+            conn.Open();
+            SqlCommand command3 = new SqlCommand("SELECT *  , (SELECT nombreusuario FROM Usuarios WHERE id = id_usuario) As Nombre FROM Comentarios WHERE id_noticia=" + id+ ";", conn);
+            using (SqlDataReader reader = command3.ExecuteReader())
+            {
+               while (reader.Read())
+                {
+                    lblComentario.Controls.Add(new Label() {
+                        Text ="</br>"+reader["Nombre"]+": "+ reader["comentario"].ToString()+"</br>"
+                        }
+                        );
+                   
+                }
+            }
+            conn.Close();
+
         }
 
         protected void lnkbCerrarSesion_Click(object sender, EventArgs e)
